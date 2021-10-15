bcda18ffc958ab66d5e2c2cced6c79bfaf8058f0
smahmud80 <selimmahmud80@gmail.com>
2021-10-15 19:09:42 +1030

    ef integration is in progress

--- ManashApi/ManashApi/Startup.cs
+++ ManashApi/ManashApi/Startup.cs
@@ -11,25 +11,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ManashApi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
         {
-            Configuration = configuration;
+            _config = config;
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options =>
-            {
-                //options.Use
-            });
+            services.AddDbContext<DataContext>(options =>options.UseSqlServer(_config.GetConnectionString("DefaultConnection")));
             services.AddControllers();
         }
 
