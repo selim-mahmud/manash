f58cf21fb2aa2d6055167eb569fcdd738eee9b2f
smahmud80 <selimmahmud80@gmail.com>
2021-10-16 20:24:51 +1030

    migration is working

--- ManashApi/ManashApi/Entities/AppUser.cs
+++ ManashApi/ManashApi/Entities/AppUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,11 @@ namespace ManashApi.Entities
 {
     public class AppUser
     {
-        public int Id { get; set; }
+        [Key]
+        public long Id { get; set; }
 
         public string UserName { get; set; }
+
+        public string Password { get; set; }
     }
 }
